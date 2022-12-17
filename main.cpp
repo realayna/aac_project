a23cbb7676e1f5ed339ac088a0d767087efb86d3
realayna <100434027+realayna@users.noreply.github.com>
2022-12-17 18:04:44 +0100

    added time of quicksort

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main()
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::cout << "size insertion_sort sorted_insertion_sort\n";
+    std::cout << "size insertion_sort sorted_insertion_sort quick_sort sorted_quicksort\n";
     for (int len = step; len < max_size; len += step)
     {
 
@@ -32,7 +32,7 @@ int main()
         nanoseconds merge(0);
         nanoseconds total_i(0);
         nanoseconds ins_s(0);
-
+        
 
         for (int i = 0; i < times; i++)
         {
@@ -49,28 +49,29 @@ int main()
             total_i += end - begin;
 
             std::sort(arr, arr + len);
-            auto begin_q = steady_clock::now();
+            auto begin_i = steady_clock::now();
             insert_sort(arr, len);
+            auto end_i = steady_clock::now();
+            ins_s += end_i - begin_i;
+
+            auto begin_q = steady_clock::now();
+            quicksort(arr_q, 0, len - 1);
             auto end_q = steady_clock::now();
-            ins_s += end_q - begin_q;
+            quick += end_q - begin_q;
 
-            // auto begin_q = steady_clock::now();
-            // quickSort(arr_q, 0, len - 1);
-            // auto end_q = steady_clock::now();
-            // quick += end_q - begin_q;
-
-            // std::sort(arr_q, arr_q + len);
-            // auto begin_qs = steady_clock::now();
-            // quickSort(arr_q, 0, len - 1);
-            // auto end_qs = steady_clock::now();
-            // s_quick += end_q - begin_q;
+            std::sort(arr_q, arr_q + len);
+            auto begin_qs = steady_clock::now();
+            quicksort(arr_q, 0, len - 1);
+            auto end_qs = steady_clock::now();
+            s_quick += end_qs - begin_qs;
         }
         std::cout
             << len << " "
 
             << total_i.count() / times << " "
-            // << quick.count() / times << " "
-           <<  ins_s.count() / times << std::endl;
+           <<  ins_s.count() / times << " "
+           << quick.count() / times << " "
+           <<s_quick.count()/times<<std::endl;
 
         delete[] arr;
     }
