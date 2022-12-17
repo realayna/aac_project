b2109da4059a98e14e20860a582251253ea7e287
realayna <100434027+realayna@users.noreply.github.com>
2022-12-17 18:04:32 +0100

    added quicksort

--- quick.h
+++ quick.h
@@ -1,8 +1,36 @@
 #ifndef QUICK_H
 #define QUICK_H
 #include <iostream>
-void quick_sort()
+#include <algorithm>
+int partition(int *arr, int f, int l)
 {
-    
+    int pivot = arr[f];
+    int pi = f + 1;
+    int temp;
+    for (auto i = f + 1; i <= l; i++)
+    {
+        if (arr[i] < pivot)
+        {
+            temp = arr[pi];
+            arr[pi] = arr[i];
+        }
+        pi++;
+    }
+    arr[f] = arr[pi - 1];
+    arr[pi - 1] = pivot;
+
+    return pi - 1;
 }
+
+void quicksort(int *arr, int f, int l)
+{
+    int pi;
+    if (f < l)
+    {
+        pi = partition(arr, f, l);
+        quicksort(arr, f, pi - l);
+        quicksort(arr, pi + 1, l);
+    }
+}
+
 #endif // QUICK_H
