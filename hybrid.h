6be6775e3de8178bb9990be269acc1b89afb7fc2
realayna <100434027+realayna@users.noreply.github.com>
2022-12-20 21:21:10 +0100

    added hybrid sort

--- hybrid.h
+++ hybrid.h
@@ -1,6 +1,37 @@
 #pragma once
-
+#include "quick.h"
+#include "insert.h"
 #include <iostream>
 #include <algorithm>
 
+void hybridsort(int *arr, int f, int l)
+{
 
+    while (f < l)
+    {
+        if (l - f < 45)
+        {
+            insert_sort(arr, l);
+            break;
+        }
+
+        else
+        {
+            int pivot = partition(arr, f, l);
+
+            if (pivot - f < l - pivot)
+            {
+                hybridsort(arr, f, pivot-1);
+                f = pivot + 1;
+            }
+
+            else
+            {
+                hybridsort(arr,pivot +1, l );
+                l = pivot - 1;
+
+            }
+        }
+    }
+
+}
