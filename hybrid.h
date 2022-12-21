2717c027cbf582fb869b74b237813c2b609353f3
realayna <ayna1204@gmai.xom>
2022-12-21 16:38:39 +0100

    fixed errors

--- hybrid.h
+++ hybrid.h
@@ -6,32 +6,20 @@
 
 void hybridsort(int *arr, int f, int l)
 {
-
-    while (f < l)
+    
+    if (l - f  < 45)
     {
-        if (l - f < 45)
-        {
-            insert_sort(arr, l);
-            break;
-        }
-
-        else
-        {
-            int pivot = partition(arr, f, l);
-
-            if (pivot - f < l - pivot)
-            {
-                hybridsort(arr, f, pivot-1);
-                f = pivot + 1;
-            }
-
-            else
-            {
-                hybridsort(arr,pivot +1, l );
-                l = pivot - 1;
-
-            }
-        }
+        insert_sort(arr, l, f);
+       
     }
+
+    else
+    {
+        int pivot = partition(arr, f, l);
+        hybridsort(arr, f, pivot-1);
+        hybridsort(arr,pivot +1, l );
+    }
+
+   
 
 }
